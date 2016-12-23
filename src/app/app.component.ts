c388ac099b65dd6ed4ecdbb420e44993fb8c6441
AJ Almaguer <ajalmaguer@gmail.com>
2016-12-22 23:51:31 -0800

    home page works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery
+var tpj = jQuery
+declare var revslider_showDoubleJqueryError
 
 @Component({
-  selector: 'tem-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'tem-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tem works!';
+export class AppComponent implements OnInit {
+	title = 'tem works!';
+
+	ngOnInit() {
+		if (tpj("#rev_slider_1_1").revolution == undefined) {
+			revslider_showDoubleJqueryError("#rev_slider_1_1");
+		} else {
+			var revapi1 = tpj("#rev_slider_1_1").show().revolution({
+				sliderType: "hero",
+				sliderLayout: "auto",
+				dottedOverlay: "none",
+				delay: 9000,
+				navigation: {},
+				visibilityLevels: [1240, 1024, 778, 480],
+				gridwidth: 1240,
+				gridheight: 820,
+				lazyType: "none",
+				parallax: {
+					type: "scroll",
+					origo: "enterpoint",
+					speed: 400,
+					levels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 46, 47, 48, 49, 50, 51, 55],
+					type: "scroll",
+				},
+				shadow: 0,
+				spinner: "spinner2",
+				autoHeight: "off",
+				disableProgressBar: "on",
+				hideThumbsOnMobile: "off",
+				hideSliderAtLimit: 0,
+				hideCaptionAtLimit: 0,
+				hideAllCaptionAtLilmit: 0,
+				debugMode: false,
+				fallbacks: {
+					simplifyAll: "off",
+					disableFocusListener: false,
+				}
+			});
+		}
+	}
 }
