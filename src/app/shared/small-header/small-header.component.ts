32089ca5c9ba2906b87797c67ddee136391713b2
AJ Almaguer <ajalmaguer@gmail.com>
2016-12-31 17:14:29 -0800

    updated small header component

--- src/app/shared/small-header/small-header.component.ts
+++ src/app/shared/small-header/small-header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'tem-small-header',
-  templateUrl: './small-header.component.html',
-  styleUrls: ['./small-header.component.css']
+	selector: 'tem-small-header',
+	templateUrl: './small-header.component.html',
+	styleUrls: ['./small-header.component.css']
 })
 export class SmallHeaderComponent implements OnInit {
+	@Input() title: string = "Sub page"
+	
+	constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
