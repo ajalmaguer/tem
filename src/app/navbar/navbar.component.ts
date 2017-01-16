a95afc1cca47980df776a96f038a82f083ef21f7
AJ Almaguer <ajalmaguer@gmail.com>
2017-01-15 16:46:42 -0800

    updated navbar to close when clicked

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare var $
 
 @Component({
-  selector: 'tem-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+	selector: 'tem-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+	constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {}
+
+	goTo(route: any[]) {
+		this.router.navigate(route)
+		window.scrollTo(0,0)
+		$(".responsive-menu-toggle").click()
+	}
 
 }
