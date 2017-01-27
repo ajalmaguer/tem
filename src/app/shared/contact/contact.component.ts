c2a3a105ed9350728d39d10915cabf653b47405b
AJ Almaguer <aj.almaguer@usaepay.com>
2017-01-26 19:54:20 -0800

    added google map.

--- src/app/shared/contact/contact.component.ts
+++ src/app/shared/contact/contact.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Http } from '@angular/http'
 import { environment } from '../../../environments/environment';
 
+declare var google
 
 @Component({
 	selector: 'tem-contact',
 	templateUrl: './contact.component.html',
 	styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit, AfterViewInit {
 	form: FormGroup
 	response: any = {
 		show: false,
@@ -20,7 +21,7 @@ export class ContactComponent implements OnInit {
 	constructor(
 		private fb: FormBuilder, 
 		private http: Http
-	) {
+		) {
 		this.form = this.fb.group({
 			name: ['', Validators.required],
 			email: ['', Validators.required],
@@ -31,7 +32,39 @@ export class ContactComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		// google_maps_5746e9b9ec998()
+		
+	}
+
+	ngAfterViewInit() {
+		console.log('hello')
+		var terminal = {lat: 37.879034, lng: -122.304205};
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 11,
+			center: terminal
+		});
+		var marker = new google.maps.Marker({
+			position: terminal,
+			map: map
+		});
+
+		var infowindow = new google.maps.InfoWindow({
+			content: `
+				<p>
+					<strong id="firstHeading" class="firstHeading">Terminal Engineering &amp; Manufacturing</strong>
+				</p>
+				<p>
+					707 Gilman Street <br>
+					Berkeley, CA 94710	
+				</p>	
+				<a href="https://goo.gl/maps/eGoDcWBo1no" target="_blank">
+					Open in google maps
+				</a>
+			`
+		})
+
+		marker.addListener('click', () => {
+			infowindow.open(map, marker)
+		})
 	}
 
 	submitForm() {
@@ -51,7 +84,7 @@ export class ContactComponent implements OnInit {
 					this.response.status = false
 					this.resetForm()
 				}
-			)
+				)
 		}
 	}
 
@@ -69,117 +102,3 @@ export class ContactComponent implements OnInit {
 	}
 
 }
-
-// declare var google
-
-// function google_maps_5746e9b9ec998() {
-	//     var latlng = new google.maps.LatLng(-33.8710, 151.2039);
-	//     var draggable = true;
-	//     var myOptions = {
-		//         zoom: 13,
-		//         center: latlng,
-		//         mapTypeId: google.maps.MapTypeId.ROADMAP,
-		//         styles: [{
-			//             "featureType": "landscape.man_made",
-			//             "elementType": "geometry",
-			//             "stylers": [{
-				//                 "color": "#f7f1df"
-				//             }]
-				//         }, {
-					//             "featureType": "landscape.natural",
-					//             "elementType": "geometry",
-					//             "stylers": [{
-						//                 "color": "#d0e3b4"
-						//             }]
-						//         }, {
-							//             "featureType": "landscape.natural.terrain",
-							//             "elementType": "geometry",
-							//             "stylers": [{
-								//                 "visibility": "off"
-								//             }]
-								//         }, {
-									//             "featureType": "poi",
-									//             "elementType": "labels",
-									//             "stylers": [{
-										//                 "visibility": "off"
-										//             }]
-										//         }, {
-											//             "featureType": "poi.business",
-											//             "elementType": "all",
-											//             "stylers": [{
-												//                 "visibility": "off"
-												//             }]
-												//         }, {
-													//             "featureType": "poi.medical",
-													//             "elementType": "geometry",
-													//             "stylers": [{
-														//                 "color": "#fbd3da"
-														//             }]
-														//         }, {
-															//             "featureType": "poi.park",
-															//             "elementType": "geometry",
-															//             "stylers": [{
-																//                 "color": "#bde6ab"
-																//             }]
-																//         }, {
-																	//             "featureType": "road",
-																	//             "elementType": "geometry.stroke",
-																	//             "stylers": [{
-																		//                 "visibility": "off"
-																		//             }]
-																		//         }, {
-																			//             "featureType": "road",
-																			//             "elementType": "labels",
-																			//             "stylers": [{
-																				//                 "visibility": "off"
-																				//             }]
-																				//         }, {
-																					//             "featureType": "road.highway",
-																					//             "elementType": "geometry.fill",
-																					//             "stylers": [{
-																						//                 "color": "#ffe15f"
-																						//             }]
-																						//         }, {
-																							//             "featureType": "road.highway",
-																							//             "elementType": "geometry.stroke",
-																							//             "stylers": [{
-																								//                 "color": "#efd151"
-																								//             }]
-																								//         }, {
-																									//             "featureType": "road.arterial",
-																									//             "elementType": "geometry.fill",
-																									//             "stylers": [{
-																										//                 "color": "#fff"
-																										//             }]
-																										//         }, {
-																											//             "featureType": "road.local",
-																											//             "elementType": "geometry.fill",
-																											//             "stylers": [{
-																												//                 "color": "black"
-																												//             }]
-																												//         }, {
-																													//             "featureType": "transit.station.airport",
-																													//             "elementType": "geometry.fill",
-																													//             "stylers": [{
-																														//                 "color": "#cfb2db"
-																														//             }]
-																														//         }, {
-																															//             "featureType": "water",
-																															//             "elementType": "geometry",
-																															//             "stylers": [{
-																																//                 "color": "#a2daf2"
-																																//             }]
-																																//         }],
-																																//         draggable: draggable,
-																																//         zoomControl: true,
-																																//         mapTypeControl: false,
-																																//         streetViewControl: false,
-																																//         scrollwheel: false
-																																//     };
-																																//     var map = new google.maps.Map(document.getElementById("google-map-area-5746e9b9ec998"), myOptions);
-																																//     var marker = new google.maps.Marker({
-																																	//         position: latlng,
-																																	//         icon: "images/home_seo2_pin.png",
-																																	//         map: map
-																																	//     });
-// }
